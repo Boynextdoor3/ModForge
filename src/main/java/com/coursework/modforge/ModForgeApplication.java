5ac64d76c45a0c4ccd412527bed6041e6f3808d8
MaxLisiy <3icestorm3@gmail.com>
2024-11-16 16:04:57 +0200

    Create and setup project

---
+++ src/main/java/com/coursework/modforge/ModForgeApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.modforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursework/modforge/ModForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.modforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
